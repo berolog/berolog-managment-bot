e2dff2f7b49298b3107e863417ecc87e04c5bc2d
RSlynko <berolog360@gmail.com>
2022-07-06 15:04:00 +0300

    mono webhook test5

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.heroku.com'
 WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
