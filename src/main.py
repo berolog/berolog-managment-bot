21f698b9b7580d169cfcd7a729a4a3ef40958df1
RSlynko <berolog360@gmail.com>
2022-07-06 18:38:26 +0300

    mono webhook test19

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 from aiogram.dispatcher.webhook import SendMessage
 from aiogram import Bot, types
-from aiogram.
 import aiogram
 
 
