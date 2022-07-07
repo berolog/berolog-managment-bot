a25dac124d0a0b890decbb873a7143debbd93f2f
RSlynko <berolog360@gmail.com>
2022-07-07 14:39:06 +0300

    mono webhook test28

--- src/main.py
+++ src/main.py
@@ -1,35 +1,63 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher.webhook import configure_app
-from aiohttp import web
-import os
 import monobank
+import logging
+import os
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram.dispatcher.webhook import configure_app, WebhookRequestHandler
+from aiogram import Bot, types
+from aiohttp import web
+
 
 MONO_TOKEN = os.getenv('MONO_TOKEN')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+app = web.Application()
+app.router.add_route('*', WEBHOOK_PATH, WebhookRequestHandler, name='webhook_handler')
+
 mono = monobank.Client(MONO_TOKEN)
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+app['BOT_DISPATCHER'] = dp
 
 
-@dp.message_handler(commands=["start"])
-async def cmd_start(message: types.Message):
-    await message.reply("start!")
+@dp.message_handler()
+async def echo(message: types.Message):
+    return SendMessage(chat_id=message.chat.id, text='Hi from webhook!',
+                       reply_to_message_id=message.message_id)
+#    await bot.get_webhook_info()
 
 
-# handle /api route
-async def api_handler(request):
-    print(web.json_response)
-    return web.json_response({"status": "OK"}, status=200)
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
 
-app = web.Application()
-# add a custom route
-app.add_routes([web.post('/api', api_handler)])
-# every request to /bot route will be retransmitted to dispatcher to be handled
-# as a bot update
-configure_app(dp, app, "/bot")
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
 
 if __name__ == '__main__':
-    web.run_app(app, port=os.getenv('PORT', 9000))
-    mono.create_webhook('https://berolog-managment-bot.herokuapp.com/api')
+    logging.basicConfig(level=logging.DEBUG)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+        app=app
+    )
+
+    mono.create_webhook(WEBHOOK_URL)
