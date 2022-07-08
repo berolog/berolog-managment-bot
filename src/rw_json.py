c12477320552e8e138cb060bf77066211e287efe
RSlynko <berolog360@gmail.com>
2022-07-08 14:35:32 +0300

    mono webhook test29

--- src/rw_json.py
+++ src/rw_json.py
@@ -2,14 +2,14 @@ import json
 
 
 def read_json():
-    with open('config.json', 'r') as openfile:
+    with open('src/config.json', 'r') as openfile:
         json_object = json.load(openfile)
 
     return json_object
 
 
 def write_json(data):
-    with open("config.json", "w") as outfile:
+    with open("src/config.json", "w") as outfile:
         json.dump(data, outfile, indent=4)
 
 
@@ -17,6 +17,4 @@ def set_limit(limit):
     data = read_json()
     data['limit'] = limit
     write_json(data)
-
-
 
